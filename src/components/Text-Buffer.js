f0d187fff17b6756be3e31a884175582ccc1ed81
leonardobrombilla <antunes.b.leonardo@gmail.com>
2025-11-23 21:25:54 -0300

    feat: added array ds

---
+++ src/components/Text-Buffer.js
@@ -0,0 +1,32 @@
+class TextBuffer  {
+    constructor() {
+        this.lines = []
+    }
+
+    getLine(index) {
+        return this.lines[index]
+    }
+
+    insertLine(index, text) {
+        this.lines.splice(index, 0, text)
+    }
+
+    deleteLine(index) {
+        const deletedElement = this.lines.splice(index, 1)
+        return deletedElement[0]
+    }
+
+    modifyLine(index, text) {
+        this.lines[index] = text
+    }
+
+    getAllLines() {
+        return [...this.lines]
+    }
+
+    getLineCount() {
+        return this.lines.length
+    }
+}
+
+export default TextBuffer;
